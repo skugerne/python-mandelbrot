e769451b5a2b4da211a29b3089e832b8612929e2
Ray Skoog <Ray.Skoog@geodata.no>
2021-10-03 14:52:43 +0200

    Added some hacky inline C for a major performance boost.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,9 +1,28 @@
 from PIL import Image
-import math
+from cffi import FFI
+
+ffi = FFI()
+ffi.set_source("inlinehack", """
+long mandlebrot(double coord_x, double coord_y) {
+    double x2;
+    double x = 0.0;
+    double y = 0.0;
+    long count = 0;
+    while( x*x + y*y <= 4.0 && count < 1000 ){
+        x2 = x*x - y*y + coord_x;
+        y = 2.0*x*y + coord_y;
+        x = x2;
+        count += 1;
+    }
+    return count;
+}
+""")
+ffi.cdef("""long mandlebrot(double,double);""")
+print("Compile...")
+ffi.compile()
+from inlinehack import lib     # import the compiled library
 
 def clr(x):
-    #v = int(round(math.log(x+1,1.03)))
-    #return (v,v,v)
     return (x%256,(x%128)*2,(x%64)*4)
 
 max_count = 1000
@@ -15,22 +34,19 @@ coordrange_x = coordmax_x - coordmin_x
 coordmin_y = -1.12
 coordmax_y = 1.12
 coordrange_y = coordmax_y - coordmin_y
-window_x = 800
+window_x = 1000
 window_y = int(round(window_x * (coordrange_y / coordrange_x)))
 im = Image.new(size=(window_x,window_y), mode='RGB', color=(0,0,0))
+prev_perc = 0
 for px in range(window_x):
+    coord_x = coordmin_x + (coordrange_x * px/window_x)
     for py in range(window_y):
-        coord_x = coordmin_x + (px/window_x * coordrange_x)
-        coord_y = coordmin_y + (py/window_y * coordrange_y)
-        x = 0.0
-        y = 0.0
-        count = 0
-        while x*x + y*y <= 4.0 and count < max_count:
-            x2 = x*x - y*y + coord_x
-            y = 2.0*x*y + coord_y
-            x = x2
-            count += 1
+        coord_y = coordmin_y + (coordrange_y * py/window_y)   # not the most efficient coordinate calculation, but should maintain more precision
+        count = lib.mandlebrot(coord_x, coord_y)
         im.putpixel((px,py),palette[count])
-    if int(1000 * px / window_x) % 10 == 0 and px: print("%d%% done" % (int(100 * px / window_x)))
+    perc = int(100 * px / window_x)
+    if perc != prev_perc:
+        print("%d%% done" % perc)
+        prev_perc = perc
 
 im.save("test.png")
